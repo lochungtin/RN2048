fd398250b78f9e755795085f350f27f58534cd02
lochungtin <lochungtin@gmail.com>
2021-05-29 12:38:08 +0100

    color typing

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -16,3 +16,29 @@ export interface ActionType {
     type: ActionName,
     payload?: any
 }
+
+// color types
+export interface ColorSchemeType {
+    bgColor: string,
+    boardColor: string,
+    tileColors: TileColorSchemeType,
+    textColor: string,
+    textboxColor: string,
+    btnColor: string,
+    accentColor: string,
+}
+
+export interface TileColorSchemeType {
+    '2': string,
+    '4': string,
+    '8': string,
+    '16': string,
+    '32': string,
+    '64': string,
+    '128': string,
+    '256': string,
+    '512': string,
+    '1024': string,
+    '2048': string,
+    large: string,
+}
